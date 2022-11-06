aa4326dfc0fa58396e60ca8caf8619ab72b1f134
unknown <kita.tomo2002@gmail.com>
2022-11-06 18:16:29 +0900

    課題33作成

--- prmn2022a/src/lecture03/Kadai33.java
+++ prmn2022a/src/lecture03/Kadai33.java
@@ -1,2 +1,17 @@
-package lecture03;public class Kadai33 {
+package lecture03;
+
+import java.util.Scanner;
+
+public class Kadai33 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("1つ目の整数を入力してください:");
+        String a = input.nextLine();
+        System.out.print("2つ目の整数を入力してください:");
+        String b = input.nextLine();
+        int num1 = Integer.parseInt(a);
+        int num2 = Integer.parseInt(b);
+        int sum = num1 + num2;
+        System.out.println(a + "+" + b + "=" + sum);
+    }
 }
