bbb65cb3591a26d85894e43b65fe3a4d1f37f2e7
unknown <kita.tomo2002@gmail.com>
2022-11-06 18:15:04 +0900

    課題31作成

---
+++ prmn2022a/src/lecture03/Kadai31.java
@@ -0,0 +1,12 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Kadai31 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("任意の文字列を入力してください:");
+        String a = input.nextLine();
+        System.out.println(a + "と入力されました");
+    }
+}
